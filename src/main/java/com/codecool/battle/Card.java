3f2d16c76b73602b5d13665a0c3c637c3240eaba
MateuszGolda <mateusz.golda1993@gmail.com>
2020-04-20 18:19:13 +0200

    move Card and CardParser files

--- src/main/java/com/codecool/battle/Card.java
+++ src/main/java/com/codecool/battle/Card.java
@@ -1,4 +1,4 @@
-
+package com.codecool.battle;
 public class Card {
     private String name;
     private int weight;
--- src/main/java/com/codecool/battle/CardParser.java
+++ src/main/java/com/codecool/battle/CardParser.java
@@ -1,3 +1,4 @@
+package com.codecool.battle;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
