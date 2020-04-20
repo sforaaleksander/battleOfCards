70932b503abcfa37ddaff2b0f0ee01915c693f58
Alek <sforaaleksander@gmail.com>
2020-04-20 22:02:26 +0200

    add ComputerPlayer class

---
+++ src/main/java/com/codecool/battle/ComputerPlayer.java
@@ -0,0 +1,9 @@
+package com.codecool.battle;
+
+public class ComputerPlayer extends Player {
+
+    @Override
+    public void chooseAttribute() {
+        // TODO choose the highest value from topCard
+    }
+}
