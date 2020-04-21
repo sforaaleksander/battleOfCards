22ffdab7acfd1eb9188b0845e52ddea554bccffd
pietia <piotrstepak@gmail.com>
2020-04-21 14:40:25 +0200

    Update CardAttribute enum

--- src/main/java/com/codecool/battle/CardAttribute.java
+++ src/main/java/com/codecool/battle/CardAttribute.java
@@ -1,15 +1,21 @@
 package com.codecool.battle;
 
 public enum CardAttribute {
-    WEIGHT(1), 
-    ROARVOLUME(2), 
-    MAXSPEED(3), 
-    NUMBEROFTEETH(4);
+    WEIGHT("Weight", 1), 
+    ROARVOLUME("RoarVolume", 2), 
+    MAXSPEED("MaxSpeed", 3), 
+    NUMBEROFTEETH("NumberOfTeeth", 4);
 
+    private final String attributeName;
     private final int value;
-
-    private CardAttribute(int value) {
+    
+    private CardAttribute(String attributeName, int value) {
+        this.attributeName = attributeName;
         this.value = value;
+    }
+
+    public String getAttributeName() {
+        return attributeName;
     }
 
     public int getValue() {
