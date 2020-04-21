8272c4fbae3e647dbff384f728836157dcc12ee0
Alek <sforaaleksander@gmail.com>
2020-04-21 12:43:55 +0200

    remove unused Playing inteface

--- src/main/java/com/codecool/battle/Playing.java
+++
@@ -1,9 +0,0 @@
-// package com.codecool.battle;
-
-// public interface Playing {
-    
-//     boolean hasCards();
-//     void chooseAttribute();
-//     String getName();
-//     Hand getHand();
-// }
