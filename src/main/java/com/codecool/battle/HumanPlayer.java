9438d579c80276bd4bcf12abae1431f0e7479885
pietia <piotrstepak@gmail.com>
2020-04-21 14:24:07 +0200

    Add chooseAtribute method

--- src/main/java/com/codecool/battle/HumanPlayer.java
+++ src/main/java/com/codecool/battle/HumanPlayer.java
@@ -1,15 +1,27 @@
 package com.codecool.battle;
 
+import java.util.Map;
+
 public class HumanPlayer extends Player {
     private IO io;
     
     public HumanPlayer(IO io){
         super();
+        this.io = io;
     }
 
     @Override
     public String chooseAttribute() {
-        // TODO
-        return null;
+        int userInput = io.gatherIntInput("Enter the attribute number you choose", 4);
+        String attribute = "Error";
+        int index = 0;
+        for (Map.Entry<String, Integer> entry : this.getHand().getTopCard().getAttributes().entrySet()) {
+            if (index == userInput) {
+                attribute = entry.getKey();
+                return attribute;
+            }
+            index++;
+        }
+		return attribute;
     }
 }
