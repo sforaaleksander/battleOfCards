abc4d92225b9ad5e6ab56f5b3a1f6747b4aa5e15
pietia <piotrstepak@gmail.com>
2020-04-21 14:34:54 +0200

    Add values to attributes

--- src/main/java/com/codecool/battle/CardAttribute.java
+++ src/main/java/com/codecool/battle/CardAttribute.java
@@ -1,5 +1,18 @@
 package com.codecool.battle;
 
 public enum CardAttribute {
-    WEIGHT, ROARVOLUME, MAXSPEED, NUMBEROFTEETH;
+    WEIGHT(1), 
+    ROARVOLUME(2), 
+    MAXSPEED(3), 
+    NUMBEROFTEETH(4);
+
+    private final int value;
+
+    private CardAttribute(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
