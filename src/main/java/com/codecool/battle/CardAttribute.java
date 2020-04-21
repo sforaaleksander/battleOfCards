2f541723e27bf6e462361a73973c96a78499946e
pietia <piotrstepak@gmail.com>
2020-04-21 15:04:13 +0200

    Fix CardAttribute enum

--- src/main/java/com/codecool/battle/CardAttribute.java
+++ src/main/java/com/codecool/battle/CardAttribute.java
@@ -1,21 +1,15 @@
 package com.codecool.battle;
 
 public enum CardAttribute {
-    WEIGHT("Weight", 1), 
-    ROARVOLUME("RoarVolume", 2), 
-    MAXSPEED("MaxSpeed", 3), 
-    NUMBEROFTEETH("NumberOfTeeth", 4);
+    WEIGHT(1), 
+    ROARVOLUME(2), 
+    MAXSPEED(3), 
+    NUMBEROFTEETH(4);
 
-    private final String attributeName;
     private final int value;
     
-    private CardAttribute(String attributeName, int value) {
-        this.attributeName = attributeName;
+    private CardAttribute(int value) {
         this.value = value;
-    }
-
-    public String getAttributeName() {
-        return attributeName;
     }
 
     public int getValue() {
