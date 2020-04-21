a4c2e833e418a615044c2387042e7dddb721ad7e
Alek <sforaaleksander@gmail.com>
2020-04-21 11:26:41 +0200

    add GameProvider loop while isRunning

--- src/main/java/com/codecool/battle/GameProvider.java
+++ src/main/java/com/codecool/battle/GameProvider.java
@@ -3,19 +3,17 @@ package com.codecool.battle;
 import java.util.HashMap;
 import java.util.Map;
 
-public class GameProvider {    
+public class GameProvider {
     private IO io;
+    private UI ui;
     private Map<String, Runnable> mainMenuMap;
-    
+    private boolean isRunning;
+
     GameProvider() {
         initialize();
-        UI ui = new UI();
-        ui.displayMenu();
-        String userChoice = io.gatherInput("");
-        mainMenuMap.get(userChoice).run();
+        handleMenu();
     }
-
-    private void createMenuMap(){
+    private void createMenuMap() {
         mainMenuMap = new HashMap<>();
         mainMenuMap.put("1", () -> playGame());
         mainMenuMap.put("2", () -> howTo());
@@ -26,20 +24,33 @@ public class GameProvider {
     private void initialize() {
         new CardParser("src/main/resources/dinosaurs.xml");
         io = new IO();
+        ui = new UI();
         createMenuMap();
+        isRunning = true;
+    }
+
+    private void handleMenu() {
+        while (isRunning) {
+            ui.displayMenu();
+            String userChoice = io.gatherInput("");
+            mainMenuMap.get(userChoice).run();
+        }
     }
 
     public void playGame() {
         int numberOfHumanPlayers = io.gatherIntInput("How many human players are there playing?", 4);
-        int numberOfComputerPlayers = io.gatherIntInput("How many computer players are there playing?", 4 - numberOfHumanPlayers);
+        int numberOfComputerPlayers = io.gatherIntInput("How many computer players are there playing?",
+                4 - numberOfHumanPlayers);
         new Game(numberOfHumanPlayers, numberOfComputerPlayers, io);
     }
 
-    public void howTo(){}
+    public void howTo() {
+    }
 
     public void about() {
     }
 
     public void exitGame() {
+        isRunning = false;
     }
 }
--- src/main/java/com/codecool/battle/Main.java
+++ src/main/java/com/codecool/battle/Main.java
@@ -3,6 +3,7 @@ package com.codecool.battle;
 public class Main {
 
     public static void main(String[] args) {
-    new GameProvider();
+
+        new GameProvider();
     }
 }
