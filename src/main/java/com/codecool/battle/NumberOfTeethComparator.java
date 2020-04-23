3ee794bc20401b35ef17a6b2e50c137ba1527e8e
Alek <sforaaleksander@gmail.com>
2020-04-23 14:55:10 +0200

    refactor numberOfTeethComparator

--- src/main/java/com/codecool/battle/NumberOfTeethComparator.java
+++ src/main/java/com/codecool/battle/NumberOfTeethComparator.java
@@ -6,12 +6,8 @@ public class NumberOfTeethComparator implements Comparator<Card> {
 
     @Override
     public int compare(Card card1, Card card2) {
-        if (card1.getAttributes().get("NUMBEROFTEETH") > card2.getAttributes().get("NUMBEROFTEETH")) {
-            return 1;
-        } else if (card1.getAttributes().get("NUMBEROFTEETH") < card2.getAttributes().get("NUMBEROFTEETH")) {
-            return -1;
-        }
-        return 0;
+        return Integer.compare(card1.getAttributes().get(CardAttribute.NUMBEROFTEETH.name()),
+                card2.getAttributes().get(CardAttribute.NUMBEROFTEETH.name()));
     }
 
 }
