dfef47a9d21ce11ffa4dd96b3a81df0706efa8ff
Alek <sforaaleksander@gmail.com>
2020-04-23 13:23:01 +0200

    fix CardParser imports

--- src/main/java/com/codecool/battle/CardParser.java
+++ src/main/java/com/codecool/battle/CardParser.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.NoSuchElementException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
