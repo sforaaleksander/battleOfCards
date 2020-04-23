588d33e43f34e6f114094cf90270466cdfad31b4
MateuszGolda <mateusz.golda1993@gmail.com>
2020-04-23 13:10:27 +0200

    delete private from enum constructor, delete commented code

--- src/main/java/com/codecool/battle/CardAttribute.java
+++ src/main/java/com/codecool/battle/CardAttribute.java
@@ -5,20 +5,11 @@ public enum CardAttribute {
 
     private final int value;
 
-    private CardAttribute(int value) {
+    CardAttribute(int value) {
         this.value = value;
     }
 
     public int getValue() {
         return value;
     }
-
-    // public static String getAttrByInt(int index) {
-    //     for (CardAttribute element : CardAttribute.values()) {
-    //         if (element.value == index) {
-    //             return element.name();
-    //         }
-    //     }
-    //     return null;
-    // }
 }
